7317590c0c2f7605fd171767dfafedb4a9b95624
ppapiruG <ppapiruG@gmail.com>
2021-11-09 09:28:09 +0900

    main css, js - modify

--- jappap-sadan/js/main.js
+++ jappap-sadan/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // header
     $('#header .bottom_nav').on({
         mouseenter: function(){
             $(this).addClass('on');
@@ -10,14 +11,7 @@ $(document).ready(function(){
     });
 });
 
-function openBtn(){
-    $('#header .left_nav .menu_nav').animate({height:'show'},350);
-}
-
-function closeBtn(){
-    $('#header .left_nav .menu_nav').animate({height:'hide'},350);
-}
-
+// body - scroll horizontally
 const scrollContainer = document.querySelector("main");
 
 scrollContainer.addEventListener("wheel", (evt) => {
@@ -25,18 +19,19 @@ scrollContainer.addEventListener("wheel", (evt) => {
     scrollContainer.scrollLeft += evt.deltaY;
 });
 
+// pinterest img layout
 window.onload = () => {
     let images = document.querySelectorAll(".image");
     let imgStack = [0, 0];
     let colWidth = 250;
     for(let i = 0; i < images.length; i++) {
-      let minIndex = imgStack.indexOf(Math.min.apply(0, imgStack));
-      let x = colWidth * minIndex;
-      let y = imgStack[minIndex];
-      imgStack[minIndex] += (images[i].children[0].height + 20);
-      images[i].style.transform = `translateX(${x}px) translateY(${y}px)`;
-      if(i === images.length - 1) {
-        document.querySelector(".images").style.height = `${Math.max.apply(0, imgStack)}px`;
-      }
+        let minIndex = imgStack.indexOf(Math.min.apply(0, imgStack));
+        let x = colWidth * minIndex;
+        let y = imgStack[minIndex];
+        imgStack[minIndex] += (images[i].children[0].height + 20);
+        images[i].style.transform = `translateX(${x}px) translateY(${y}px)`;
+        if(i === images.length - 1) {
+            document.querySelector(".images").style.height = `${Math.max.apply(0, imgStack)}px`;
+        }
     }
-  }
+}
